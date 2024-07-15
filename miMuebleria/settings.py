f3656fb0c7b5298382cfc3c40610ecb385603061
Inti Garcia Salgado <igarsal2008@gmail.com>
2024-07-15 12:41:17 -0600

    Corrección de rutas

--- miMuebleria/settings.py
+++ miMuebleria/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import environ
+from pathlib import Path
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminApp')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Inicializar django-environ
 env = environ.Env(
@@ -144,6 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 MEDIA_URL= '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
