2e31fe8a1b3c7133d29d276bc50f5d13820b6aa7
IGARSAL <igarsal2008@gmail.com>
2024-07-16 18:14:46 -0600

    modificaciones del inicio

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.conf import settings
 from tienda.views import TiendaView
+from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     path('', TiendaView.as_view(), name='tienda'),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
