c15b3910ae51e7600470212321efcf73b1efbb37
IGARSAL <igarsal2008@gmail.com>
2024-07-18 13:03:39 -0600

    Inicio de carro

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\inti.garcia.fgr\documents\mimuebleria\.venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
