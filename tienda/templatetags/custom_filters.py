8de00c1e123fb6e55cb0676003b437b680d1331f
IGARSAL <igarsal2008@gmail.com>
2024-07-19 12:48:16 -0600

    cambio de tienda

--- tienda/templatetags/custom_filters.py
+++ tienda/templatetags/custom_filters.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def mul(value, arg):
     return float(value) * float(arg)
+
+@register.filter
+def div(value, arg):
+    return float(value) / float(arg)
