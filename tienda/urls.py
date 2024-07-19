42560f4659a5b3cfa2519ebe07aa06d7a6966b2c
IGARSAL <igarsal2008@gmail.com>
2024-07-18 18:53:37 -0600

    funcionamiento del carrito

--- tienda/urls.py
+++ tienda/urls.py
@@ -7,10 +7,4 @@ from django.urls import path
 urlpatterns = [
     path('', TiendaView.as_view(), name='tienda'),
     path('productos/', ProductosJsonView.as_view(), name='productos_json'),
-]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
